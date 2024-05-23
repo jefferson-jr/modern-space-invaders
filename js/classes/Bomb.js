80591f2e0564ea575a14b72aa755f26a58cf33e1
Jefferson junior <jeffersonjunior.amorim@gmail.com>
2024-05-23 10:27:00 -0400

    add draw function

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -12,4 +12,15 @@ class Bomb {
         radius: 30
     });
    }
+
+   draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+   }
 }
