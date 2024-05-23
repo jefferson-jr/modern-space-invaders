2ca4655347dc7b2e0c1365e036e0658c2041105b
Jefferson junior <jeffersonjunior.amorim@gmail.com>
2024-05-23 11:25:02 -0400

    add PowerUp class

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -58,3 +58,11 @@ class Bomb {
         })
    }
 }
+
+class PowerUp {
+    constructor({position, velocity,}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 15;
+    }
+}
