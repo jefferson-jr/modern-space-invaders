fa2b1cc78c9ab7589786b4734c71a56479e8c732
Jefferson junior <jeffersonjunior.amorim@gmail.com>
2024-05-28 11:07:39 -0400

    add shoot function in invader class

--- js/classes/Invader.js
+++ js/classes/Invader.js
@@ -20,7 +20,7 @@ class invader {
     };
   }
 
-draw() {
+  draw() {
     c.drawImage(
       this.image,
       this.position.x,
@@ -28,13 +28,29 @@ draw() {
       this.width,
       this.height
     );
-}
+  }
 
-update({ velocity}){
-    if(this.image) {
-        this.draw();
-        this.position.x += velocity.x;
-        this.position.y += velocity.y;
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
     }
-}   
+  }
+
+  shoot(invaderProjectiles) {
+    audio.enemyShoot.play();
+    invaderProjectiles.push(
+      new InvaderProjectile({
+        position: {
+          x: this.position.x + this.width / 2,
+          y: this.position.y + this.height
+        },
+        velocity: {
+          x: 0,
+          y: 5
+        }
+      })
+    );
+  }
 }
