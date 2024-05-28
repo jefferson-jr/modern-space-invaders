c2a274edb445176525a3fee184629d7d87a4e480
Jefferson junior <jeffersonjunior.amorim@gmail.com>
2024-05-28 11:15:19 -0400

    add draw function  InvaderProjectile class

--- js/classes/InvaderProjectile.js
+++ js/classes/InvaderProjectile.js
@@ -3,7 +3,11 @@ class invaderProjectile {
         this.positiion = positiion;
         this.velocity = velocity;
         this.width = 3;
-        this.height = 10;
-        
+        this.height = 10;     
+    }
+    
+    draw() {
+        c.fillStyle = "white";
+        c.fillRect(this.positiion.x, this.positiion.y, this.width, this.height);
     }
 }
