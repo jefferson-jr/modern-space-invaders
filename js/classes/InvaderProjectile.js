8706234d64a047f01aa7fa45657a6c3621600eaa
Jefferson junior <jeffersonjunior.amorim@gmail.com>
2024-05-28 11:12:58 -0400

    add InvaderProjectile class

---
+++ js/classes/InvaderProjectile.js
@@ -0,0 +1,9 @@
+class invaderProjectile {
+    constructor({ positiion, velocity}){
+        this.positiion = positiion;
+        this.velocity = velocity;
+        this.width = 3;
+        this.height = 10;
+        
+    }
+}
