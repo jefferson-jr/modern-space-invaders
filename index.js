805f0c38b23df741538463940484c168fa647e10
Jefferson junior <jeffersonjunior.amorim@gmail.com>
2024-06-13 08:28:21 -0400

    initialized the creation of the animate function

--- index.js
+++ index.js
@@ -111,3 +111,19 @@ createParticles ({
     fades: true
 });
 }
+
+function animate() {
+    if (!game.active) return;
+    requestAnimationFrame(animate);
+
+    const msNow = window.performance.now();
+    const elapsed = msNow - msPrev;
+
+    if(elapsed < fpsInterval) return;
+
+    msPrev = msNow - (elapsed % fpsInterval);
+
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    
+}
